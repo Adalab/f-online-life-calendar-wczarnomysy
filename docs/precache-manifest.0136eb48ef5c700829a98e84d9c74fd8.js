eb4b5d67da6258617e2c6c47a0016695dfcaa3ad
wczarnomysy <w.czarnomysy@gmail.com>
2019-07-03 12:17:07 +0200

    Last changes

---
+++ docs/precache-manifest.0136eb48ef5c700829a98e84d9c74fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc38518532d8d9c04b9d03cccca1ef2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "561141a2683272aab1ed",
+    "url": "./static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "a92f8902a9a50e65ee4b",
+    "url": "./static/css/main.dc9f7bc9.chunk.css"
+  },
+  {
+    "revision": "561141a2683272aab1ed",
+    "url": "./static/js/2.a281a352.chunk.js"
+  },
+  {
+    "revision": "a92f8902a9a50e65ee4b",
+    "url": "./static/js/main.c33e2dd1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
